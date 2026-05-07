bf49a406266be6f6f21994a88f572af979022029
rckhz <alejandrolima123@gmail.com>
2026-05-06 23:09:40 -0300

    falta sua parte dudi

--- calculadora/Controllers/CalculadoraController.cs
+++ calculadora/Controllers/CalculadoraController.cs
@@ -1,6 +1,26 @@
-﻿namespace calculadora.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using calculadora.Models;
+
+namespace calculadora.Controllers
 {
-    public class CalculadoraController
+    public class CalculadoraController : Controller
     {
+        // nome da pagina que vai ser chamada
+        public IActionResult Index()
+        {
+            //fala pro asp.net para mostrar a tela html pro usuario
+            return View();
+        }
+
+        // diz que a acao so rodaa quando o usuario clicar no botao de calcular
+        [HttpPost]
+        //recebe os dados que o usuario digito na tela - numero1, numero2 e operador
+        public IActionResult Index(CalculadoraModel model) 
+        { 
+            //chama o metodo model que vc escreveu - aqui comeca a conta
+            model.Calcular();
+            //devolve pro usuario o resultado daa conta
+            return View(model);
+        }
     }
 }
---
+++ calculadora/Views/Calculadora/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace calculadora.Views.Calculadora
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
